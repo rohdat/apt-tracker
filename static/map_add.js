f824f86329ed54a57c7e4afa96550f40ad024342
rohdat <rohitdate86@gmail.com>
2016-05-25 12:31:38 -0700

    info window is operational

--- static/map_add.js
+++ static/map_add.js
@@ -1,40 +1,69 @@
+var infowindow = null;
 function initMap() {
 	// Will get lat and lon based on written address
 	var addr = 'google hq';
 	var mapDiv = 'map';
+	idmap = 'map';
+	mapDiv = document.getElementById(idmap);
+	mapDiv.style.width='1000px';
+	mapDiv.style.height='250px';
+	mapDiv.style.backgroundColor='#CCC';
+	var markers = [];
+	var bound = new google.maps.LatLngBounds();
 	$.getJSON('/main/JSON',function(json) {
 		json.Apt.forEach(function (a){
 			// console.log(a.id);
 			addr = a.addr;
 			console.log(addr);
-			idmap = 'map-'+a.id;
-			mapDiv = document.getElementById(idmap);
-			mapDiv.style.width='250px';
-			mapDiv.style.height='250px';
-			mapDiv.style.backgroundColor='#CCC';
-			geocodeMe(addr, mapDiv, function (latlon, mapDiv){
-				var map = new google.maps.Map(mapDiv, {
-					// center: {lat:44.54, lng:-78.56},
-					center:latlon,
-					zoom:14
-				});
+			geocodeMe(addr, function (latlon){
+				bound.extend(latlon);
+				console.log("During for loop:");
+				console.log(bound.getCenter().toJSON());
+				var contentString = getContent(a);
 				var marker = new google.maps.Marker ({
 					map:map,
 					position:latlon
 				});
+
+				marker.addListener('click',function() {
+					if (infowindow){
+						infowindow.close();
+					}
+					infowindow = new google.maps.InfoWindow({
+						content: contentString
+					});
+
+					infowindow.open(map,marker);
+				})
+				updateMap();
 			})
 		})
 	})
+	var mapCenter = bound.getCenter();
+	var map = new google.maps.Map(mapDiv, {
+		center: mapCenter,
+		zoom:14
+	});
+	console.log(bound.getCenter().toJSON());
+	
+	var updateMap = function (){
+		mapCenter = bound.getCenter();
+		map.setCenter(mapCenter);
+		map.fitBounds(bound);
+	};
+	var getContent = function (a) {
+		return "<div>Rent: $"+a.rent+"</div><hr><div>Status: "+a.link;
+	}
 	
 }
 
-function geocodeMe (addr, mapDiv, callback) {
+function geocodeMe (addr, callback) {
 	geocoder = new google.maps.Geocoder();
 	geocoder.geocode( { 'address': addr }, function (results, status) {
 	if (status == google.maps.GeocoderStatus.OK) {
 		latlon = new google.maps.LatLng(results[0].geometry.location.lat(), results[0].geometry.location.lng());
 		if (typeof callback == 'function') {
-			callback(latlon, mapDiv);
+			callback(latlon);
 		}
 		// console.log(results[0].geometry.location.lat())
 	} else {
